408fef6aaaffef586e217b7d5fa3dcaa9aa33319
hitcherland <hitcherland@gmail.com>
2022-09-30 13:41:41 +0100

    Update for FoundryVTT 9.x,10.x

--- module/tile.js
+++ module/tile.js
@@ -1,49 +1,10 @@
 export class BHTTile extends Tile {
-
-    _getTileOffset() {
-        const aw = Math.abs(this.data.width);
-        const ah = Math.abs(this.data.height);
-        const apo = 0.5 / Math.tan(Math.PI / 6);
-        if ([CONST.GRID_TYPES.HEXODDQ, CONST.GRID_TYPES.HEXEVENQ].includes(canvas.grid.type)) {
-            return [aw / 2, ah * apo / 2];
-        } else if ([CONST.GRID_TYPES.HEXODDR, CONST.GRID_TYPES.HEXEVENR].includes(canvas.grid.type)) {
-            return [aw * apo / 2, ah / 2];
-        } else {
-            return [aw / 2, ah / 2];
+    _refresh(options) {
+        super._refresh(options);
+        if ( this.mesh ) {
+            let [ox, oy] = this._getTileOffset();
+            console.warn(this, this.document.x, this.document.y, ox, oy);
+            this.mesh.position.set(this.document.x + canvas.grid.grid.w / 2, this.document.y + canvas.grid.grid.h / 2);
         }
-    }
-
-    refresh() {
-        const r = Math.toRadians(this.data.rotation);
-
-        // Update tile appearance
-        this.position.set(this.data.x, this.data.y);
-        if (this.tile) {
-
-            // Tile position
-            this.tile.scale.x = this.data.width / this.texture.width;
-            this.tile.scale.y = this.data.height / this.texture.height;
-            this.tile.position.set(...this._getTileOffset());
-            this.tile.rotation = r;
-
-            // Tile appearance
-            this.tile.alpha = this.data.hidden ? Math.min(0.5, this.data.alpha) : this.data.alpha;
-            this.tile.tint = this.data.tint ? foundry.utils.colorStringToHex(this.data.tint) : 0xFFFFFF;
-        }
-
-        // Temporary tile background
-        if (this.bg) this.bg.clear().beginFill(0xFFFFFF, 0.5).drawRect(0, 0, this.data.width, this.data.height).endFill();
-
-        // Define bounds and update the border frame
-        let bounds = (this.data.width === this.data.height) ?
-            new NormalizedRectangle(0, 0, this.data.width, this.data.height) : // Square tiles
-            NormalizedRectangle.fromRotation(0, 0, this.data.width, this.data.height, r); // Non-square tiles
-        this.hitArea = this._controlled ? bounds.clone().pad(20) : bounds;
-        this._refreshBorder(bounds);
-        this._refreshHandle(bounds);
-
-        // Set visibility
-        this.visible = !this.data.hidden || game.user.isGM;
-        return this;
     }
 }
