e9c91b31870e459ef2b0dba0b4c9dd8dba6a3a60
hitcherland <hitcherland@gmail.com>
2022-10-06 19:37:36 +0100

    Update tile.js
    Remove calls to function that is no longer used.

--- module/tile.js
+++ module/tile.js
@@ -2,8 +2,6 @@ export class BHTTile extends Tile {
     _refresh(options) {
         super._refresh(options);
         if ( this.mesh ) {
-            let [ox, oy] = this._getTileOffset();
-            console.warn(this, this.document.x, this.document.y, ox, oy);
             this.mesh.position.set(this.document.x + canvas.grid.grid.w / 2, this.document.y + canvas.grid.grid.h / 2);
         }
     }
