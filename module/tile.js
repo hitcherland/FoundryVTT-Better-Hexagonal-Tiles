6a7e152c618721c5b095ea11cb11edaa73b0a0ae
hitcherland <hitcherland@gmail.com>
2023-02-09 20:02:01 +0000

    Only enable when canvas grid is in hexmode

--- module/tile.js
+++ module/tile.js
@@ -1,7 +1,7 @@
 export class BHTTile extends Tile {
     _refresh(options) {
         super._refresh(options);
-        if ( this.mesh ) {
+        if ( this.mesh && game.canvas.grid.isHex ) {
             this.mesh.position.set(this.document.x + canvas.grid.grid.w / 2, this.document.y + canvas.grid.grid.h / 2);
         }
     }
